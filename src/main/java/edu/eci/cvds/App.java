3400a16765ccd6d1167cb0894ac126ac4790db38
unknown <wilson.delgado@labinfo.is.escuelaing.edu.co>
2022-08-20 09:44:17 -0500

    First Commit

---
+++ src/main/java/edu/eci/cvds/App.java
@@ -0,0 +1,15 @@
+package edu.eci.cvds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String Arg1 = args[0];
+        String Arg2 = args[1];
+        System.out.println(Arg1 + Arg2);
+    }
+}
